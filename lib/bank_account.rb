ba186e42ca3840eb0c1a257d39ba0b6c798ca314
Marty Warfel <marthawarfel@gmail.com>
2020-12-13 16:15:14 -0600

    commit tree 83f1653711543ef060eae9e4cc4ea2b19d2fd0fa

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,28 @@
 class BankAccount
 
 end
+    attr_reader :name
+    attr_accessor :balance, :status
+
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+    end
+
+    def deposit(deposit)
+        @deposit = deposit
+        @balance += deposit
+    end
+
+    def display_balance
+        return "Your balance is $#{@balance}."
+    end
+
+    def valid?
+        @status == "open" && @balance > 0
+    end
+
+    def close_account
+        @status = "closed"
+    end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,38 @@
+require 'pry'
+require_relative 'bank_account.rb'
+
 class Transfer
   # your code here
+
+  @@count = 1
+
+  attr_accessor :sender, :receiver, :amount, :status, :balance
+  attr_reader :name
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    self.sender.valid? && self.receiver.valid?
+  end
+
+  def execute_transaction
+        self.sender.balance -= self.amount
+        self.receiver.balance += self.amount
+        self.status = "complete"
+        @@count -= 1
+
+      end
+
 end
+
+
+marty = BankAccount.new("Marty")
+evan = BankAccount.new("Evan")
+transfer = Transfer.new(marty, evan, 500)
+
+#binding.pry 
